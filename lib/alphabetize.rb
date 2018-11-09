f22cfbd2b90c7005e3352764e2a8031f4a6fb079
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-09 09:46:58 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ def alphabetize(arr)
   # code here
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   alphabet = esperanto_alphabetalphabet.chars 
-    arr.each do |word| 
+    arr.sort_by do |word| 
       index1 = alphabet.index(word[0])
       index2 = alphabet.index
       if word[0]
